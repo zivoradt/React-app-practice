17e7745a14d4ff53a4701b571ba1efaa1ef709de
zivoradt <zivoradtrickovic@gmail.com>
2023-02-03 11:20:54 +0100

    created front-end v.1

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+
+// Commont Componenets
+import Header from './components/header';
+
+// Content Components
+import Home from './content/home';
+
+// Styles and fonts
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Header></Header>
+
+      {/*Client-Side Routing */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+      </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
