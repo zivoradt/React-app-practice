7ebe4e56ff9bdfffe0472bcee4b9b4b40653c4b4
zivoradt <zivoradtrickovic@gmail.com>
2023-02-05 16:17:24 +0100

    added movie model

--- Front-end/src/components/http-common.tsx
+++ Front-end/src/components/http-common.tsx
@@ -5,7 +5,7 @@
 import axios from "axios";
 export default axios.create({
     baseURL: "http://localhost:3500/api",
-    // If URL is on Heroku if will be like: http://react-app.com/api
+    
     headers:{
         "Content-type": "application/json"
     }
